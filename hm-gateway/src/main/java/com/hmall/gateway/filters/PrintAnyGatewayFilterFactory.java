47c3fec19eed09ea9ba56127d3941879b3fc2609
git_ku <1176061678@qq.com>
2026-03-03 00:26:55 +0800

    【新增功能】gateway过滤器新增自定义参数要加这些

--- hm-gateway/src/main/java/com/hmall/gateway/filters/PrintAnyGatewayFilterFactory.java
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/PrintAnyGatewayFilterFactory.java
@@ -1,5 +1,6 @@
 package com.hmall.gateway.filters;
 
+import lombok.Data;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
@@ -8,16 +9,39 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Component
-public class PrintAnyGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+//把Objerct变成自己定义的参数类
+public class PrintAnyGatewayFilterFactory extends AbstractGatewayFilterFactory<PrintAnyGatewayFilterFactory.Config> {
     @Override
-    public GatewayFilter apply(Object config) {
+    public GatewayFilter apply(Config config) {
         return new OrderedGatewayFilter(new GatewayFilter() {
             @Override
             public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                System.out.println(config.getA() +""+ config.getB() +""+ config.getC());
                 System.out.println("print any filter running");
                 return chain.filter(exchange);
             }
         },1);
+    }
+
+    //定义自己filter的参数
+    @Data
+    public static class Config{
+        private String a;
+        private String b;
+        private String c;
+    }
+
+    //把内部参数类写到构造方法
+    public PrintAnyGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    //新增变量顺序
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return List.of("a","b","c");
     }
 }
